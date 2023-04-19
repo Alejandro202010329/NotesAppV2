3d0e19b6701416cedc590c88a44b928464d7c818
Miguel Alejandro Acosta Quezada <202010329@itla.edu.do>
2023-04-18 20:30:08 -0400

    App de notas en react

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
 from fastapi import FastAPI, Body
+from fastapi.middleware.cors import CORSMiddleware
 from database import db
+
 app = FastAPI()
 
-@app.get('/')
-def getRoute():
-    return['/notes', '/notes/<pk>']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get('/notes')
+@app.get("/")
+def getRoutes():
+    return ['/notes', '/notes/<ID>']
+
+
+@app.get("/notes")
 def getNotes():
-    # notes = db.search_by_value('notesapp', 'notes', 'id', '*', get_attributes=['*'])
     notes = db.sql('SELECT * FROM notesapp.notes ORDER BY __updatedtime__ DESC')
+    #notes = db.search_by_value('notesapp', 'notes', "id", "*", get_attributes=['*'])
     return notes
 
-@app.get('/notes/{id}')
-def getNote(pk:str):
-    notes = db.search_by_hash('notesapp', 'notes', [pk])
+@app.get("/notes/{id}")
+def getNote(id:str):
+    notes = db.search_by_hash('notesapp', 'notes', [id] , get_attributes=['*'] )
     return notes[0]
 
-@app.post('/notes')
-def addNote(data = Body()):
-    print('Data:', data)
+@app.post("/notes")
+def addNotes(data = Body()):
     db.insert('notesapp', 'notes', [{"body":data['body']}])
-    notes = db.search_by_value('notesapp', 'notes', 'id', '*', get_attributes=['*'])
-
+    notes = db.sql('SELECT * FROM notesapp.notes')
     return notes
 
-@app.put('/notes/{pk}')
-def updateNote(pk:str, data = Body()):
-    db.update('notesapp', 'notes', [{"id":pk, "body":data["body"]}])
-    notes = db.search_by_value('notesapp', 'notes', 'id', '*', get_attributes=['*'])
+
+@app.put("/notes/{id}")
+def updateNote(id:str, data = Body()):
+    note = db.update('notesapp', 'notes', [{"id":id, "body":data["body"]}])
+    notes = db.sql('SELECT * FROM notesapp.notes')
     return notes
 
-@app.delete('/notes/{pk}')
-def deleteNote(pk:str):
-    db.delete('notesapp', 'notes', [pk])
-    notes = db.search_by_value('notesapp', 'notes', 'id', '*', get_attributes=['*'])
+@app.delete("/notes/{id}")
+def deleteNote(id:str):
+    db.delete('notesapp', 'notes', [id])
+    notes = db.sql('SELECT * FROM notesapp.notes')
     return notes
